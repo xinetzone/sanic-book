898c12950504fad4c8e3de86c06423f8f26804c9
xinetzone <xinzone@outlook.com>
2022-12-15 23:59:07 +0800

    更新

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,17 @@
-FROM tiangolo/uwsgi-nginx:python3.11
-COPY ./requirements.txt /app/requirements.txt
-RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
-COPY ./app /app
+# 使用官方 Python 轻量级镜像
+# https://hub.docker.com/_/python
+FROM python:3.8-slim
+
+# 将本地代码拷贝到容器内
+ENV APP_HOME /app
+WORKDIR $APP_HOME
+COPY . ./
+
+# 安装依赖
+RUN pip install sanic
+
+# 启动 Web 服务
+# 这里我们使用了 gunicorn 作为 Server，1 个 worker 和 8 个线程
+# 如果您的容器实例拥有多个 CPU 核心，我们推荐您把线程数设置为与 CPU 核心数一致
+# CMD exec gunicorn --bind :80 --workers 1 --threads 8 --timeout 0 main:app
+CMD exec sanic server:app --host=0.0.0.0 --port=1337 --fast --workers=4
--- server.py
+++ server.py
@@ -1,45 +1,12 @@
 from sanic import Sanic
-from sanic.request import Request
-from sanic.response import HTTPResponse, text, html
-import aiofiles
-# import asyncio
-# import uvloop
-# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+from sanic.response import text
 
-# 创建 Sanic 实例对象, Sanic 是 web 服务的入口类, 它等价于 flask.Flask
-# 然后里面传递字符串作为应用的名字
-app = Sanic("my_awesome_server")
-# app.config 是 Config 对象, 可以存储相应的配置, 有以下几种加载方式
-app.config.user_name = "xinetzone"  # 通过属性查找的方式设置
-
+app = Sanic("MyHelloWorldApp")
 
 @app.get("/")
-async def hello_world(request: Request) -> HTTPResponse:
-    lst = []
-    lst.append(f"请求的url: {request.url}")
-    lst.append(f"请求的path: {request.path}")
-    lst.append(f"请求的方法: {request.method}")
-    lst.append(f"请求的查询参数: {request.query_args}")
-    lst.append(f"请求的查询参数: {request.args}")
-    lst.append(f"a: {request.args.get('a')}, "
-               f"b: {request.args.get('b')}, "
-               f"a_lst: {request.args.getlist('a')}, "
-               f"c: {request.args.get('c')}, "
-               f"c: {request.args.get('c', 'xxx')}")
-    return text("\n".join(lst))
-
-@app.reload_process_start
-async def reload_start(*_):
-    print(">>>>>> reload_start <<<<<<")
-
-
-@app.main_process_start
-async def main_start(*_):
-    print(">>>>>> main_start <<<<<<")
-
-
-app.register_listener(reload_start, "before_server_start")
+async def hello_world(request):
+    return text("Hello, world.")
 
 
 if __name__ == "__main__":
-    app.run(dev=True)
+    app.run(dev=True, host='0.0.0.0', port=80)
