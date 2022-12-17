264d2b9c5514707a3f30c1c9ae00d83206391c46
xinetzone <xinzone@outlook.com>
2022-12-17 11:09:09 +0800

    modified:   app/requirements.txt

--- app/_config.py
+++ app/_config.py
@@ -1,5 +1,4 @@
 from typing import Any
-from sanic import Sanic, text
 from sanic.config import Config
 import toml
 
--- app/server.py
+++ app/server.py
@@ -1,14 +1,27 @@
-from sanic import Sanic
+import time
+import asyncio
+from sanic import Sanic, Request, HTTPResponse
 from sanic.response import text
 from _config import TomlConfig
 
+
+class NanoSecondRequest(Request):
+    @classmethod
+    def generate_id(*_):
+        return time.time_ns()
+
 toml_config = TomlConfig(path="./configs/main.toml")
 app = Sanic(toml_config.APP_NAME, config=toml_config)
+app.config.FORWARDED_SECRET = "super-duper-secret"
+app.config.REAL_IP_HEADER = "CF-Connecting-IP"
+app.config.PROXIES_COUNT = 2
 
 
 @app.get("/")
-async def hello_world(request):
-    return text("Hello, world.")
+async def hello_world(request: Request) -> HTTPResponse:
+    # return text("Hello, world.")
+    print("IP: ", request.remote_addr)
+    return text(f"{request.id}\n{request.remote_addr}")
 
 
 if __name__ == "__main__":
